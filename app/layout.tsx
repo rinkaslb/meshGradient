3d889758b02738ff2decca9e5ab729c9654c6e42
Luis Gil <luis.1.gil@gmail.com>
2026-02-05 23:18:42 +0100

    Add 'dark' class to HTML element

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" class="dark">
       <body className="font-sans antialiased">{children}</body>
     </html>
   )
